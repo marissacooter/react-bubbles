249c1cb9ce636de3df85fd28a3a40142beab83ef
Marissa <marissacooterl@gmail.com>
2020-04-24 11:15:05 -0500

    created login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,45 +6,64 @@ class Login extends React.Component {
     super(props);
     this.state = {
       credentials: {
-        username: '',
-        password:'',
+        username: "",
+        password: "",
       },
       isLoading: false,
     };
   }
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({
       credentials: {
         ...this.state.credentials,
-        [e.target.name]: e.target.value
+        [e.target.name]: e.target.value,
       },
     });
-    console.log('credentials', this.state.credentials);
+    console.log("credentials", this.state.credentials);
   };
 
-  loginHandler = e => {
+  loginHandler = (e) => {
     e.preventDefault();
     this.setState({
       isLoading: true,
     });
-    axios 
-      .post('http://localhost:5000/api/login', this.state.credentials)
-      .then(res => {
-        localStorage.setItem('token', JSON.stringify(res.data.payload));
-        this.props.history.push('/bubblepage');
+    axios
+      .post("http://localhost:5000/api/login", this.state.credentials)
+      .then((res) => {
+        localStorage.setItem("token", JSON.stringify(res.data.payload));
+        this.props.history.push("/bubblepage");
         this.setState({
           isLoading: false,
         });
       })
-      .catch(err => console.log({err}));
+      .catch((err) => console.log({ err }));
   };
 
   render() {
     return (
       <>
-        <h1>Welcome to the Bubble App!</h1>
-        <p>Build a login page here</p>
+        <h1>Welcome, Please Log In!</h1>
+        <div class="login">
+          {this.state.isLoading && <p>Loading! One Moment ..</p>}
+          <form onSubmit={this.loginHandler}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter Username"
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="password"
+              placeholder="Enter Password"
+              value={this.state.credentials.password}
+              onChange={this.handleChange}
+            />
+            <button>Log In</button>
+          </form>
+        </div>
       </>
     );
   }
