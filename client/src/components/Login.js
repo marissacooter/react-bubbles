7291cd5c62cd73251e01bbfd6f9a4a47eb6006da
Marissa <marissacooterl@gmail.com>
2020-04-24 10:24:03 -0500

    converting from functional component to class component in login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,17 @@
 import React from "react";
+import axios from "axios";
 
-const Login = () => {
+class Login extends React.Component {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+  render() {
+    return (
+      <>
+        <h1>Welcome to the Bubble App!</h1>
+        <p>Build a login page here</p>
+      </>
+    );
+  }
+}
 
 export default Login;
